05cd4c702daea19a822e88e97eefb461248a1ad5
Bendzina <165944559+Bendzina@users.noreply.github.com>
2024-11-04 20:50:19 +0400

    init

--- blog/views.py
+++ blog/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'blog/index.html')
 
 def about(request):
-    return render(request, 'about.html')
+    return render(request, 'blog/about.html')
 
 def base(request):
     return render(request, 'base.html')
